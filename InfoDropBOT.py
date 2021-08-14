e094d5c7d638f679c864d976f69e20f7f1656589
SUMANT HEGDE <52189454+KILROY007@users.noreply.github.com>
2021-08-14 10:57:54 +0530

    Add files via upload

--- InfoDropBOT.py
+++ InfoDropBOT.py
@@ -13,17 +13,7 @@ def findat(msg):
 
 @bot.message_handler(commands=['start']) 
 def send_welcome(message):
-    bot.reply_to(message, 'Hi!There Welcome To Department Of ISE-BOT')
-    
-@bot.message_handler(commands=['/Lab-Manuals'])
-def send_welcome(message)
-    bot.reply_to(message, 'Select the seminster')
-    bot.reply_to(message,'/3rd-lab \n /4th-lab \n /5th-lab \n /6th-lab \n /7th-lab')
-
-@bot.message_handler(commands=['/3rd-lab'])
-def send_welcome(message)
-    bot.reply_to(message, 'https://cloud.ewitise.org.in/index.php/s/9cWNXqQSCLFZcSm?path=%2FLab%20Manuals%2F3rd%20Sem')
-
+    bot.reply_to(message, 'Hi!There Welcome To Department Of ISE-BOT  Type /help Now')
 
 @bot.message_handler(commands=['help']) 
 def send_help(message):
