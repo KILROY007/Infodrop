0d3d8793593f07f82dcc76a142becd3b24b5cc89
SUMANT HEGDE <52189454+KILROY007@users.noreply.github.com>
2021-08-14 10:05:28 +0530

    Add files via upload

---
+++ InfoDropBOT.py
@@ -0,0 +1,46 @@
+import os
+from flask import Flask, request
+import telebot
+
+TOKEN = "1310851601:AAHAbO1LKPZkaV0a6m_rVV2ZkvOAUsfHJmY"
+bot = telebot.TeleBot(token=TOKEN)
+server = Flask(__name__)
+
+def findat(msg):
+    for i in msg:
+        if '@' in i:
+            return i
+
+@bot.message_handler(commands=['start']) 
+def send_welcome(message):
+    bot.reply_to(message, 'Hi!There Welcome To Department Of MY BOT  Type /help Now')
+
+@bot.message_handler(commands=['help']) 
+def send_help(message):
+    bot.reply_to(message, "Type The Name Of The Of The Subject for example[*@DBMS*]")
+
+@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
+def at_converter(message):
+    texts = message.text.split()
+    at_text = findat(texts)
+    if at_text == '@': 
+        pass
+    else:
+        drive_link = "https://patient-union-0c47.kilroy1969.workers.dev/0:search?q={}".format(at_text[1:])
+        bot.reply_to(message, drive_link)
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://infodrop007.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
